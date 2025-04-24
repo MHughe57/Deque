f75f759a4cf95ce743e85543e42920984d0fedef
McKenzie Hughes <mhughe57@ut.utm.edu>
2025-04-23 20:43:18 -0500

    Good for now

--- main.cpp
+++ main.cpp
@@ -10,21 +10,42 @@ int main(){
   cout << "Push Test" << endl;
 
   //push front
-  for (int i = 100; i <= 150; ++i){
+  for (int i = 1; i <= 50; ++i){
     A.push_front(i);
     cout << "Pushed to the front ( " << i << " )" << endl;
   }
 
   //push back
   cout << "Pushing Back" << endl;
-  for (int i = 200; i <= 230; ++i){
+  for (int i = 1; i <= 30; ++i){
     A.push_back(i);
-    cout << "Pushed to the back ( " << i << " )" <<endl;
+    cout << "Pushed to the back ( " << i << " )" << endl;
   }
 
+
+  //Operator in use to output entire map
+  cout << "Elements currently: " << endl;
+  for (int i = 0; i < A.size(); ++i){
+    cout << i << ": " << d[i] << endl;
+  }
+
+  
   cout << "Current num of elements: " << A.size << endl;
   cout << "Element in front: "<< A.front() << endl;
   cout << "Element in back: " << A.back() << endl;
-  
-  
+
+
+  //pop test
+  cout << "Pop Test" << endl;
+  for (int i = 1; i <= 15; i++){
+    A.pop_front(i);
+    A.pop_back(i);
+  }
+
+  cout << "New Size: " A.size();
+  cout << "New front: " << A.front() << endl;
+  cout << "New back: " << A.back() << endl;
+
+  cout << "Testing Conplete" << endl;
+  return 0;
 }
